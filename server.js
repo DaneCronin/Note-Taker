19e8fa1ad1188de36df66ff3dae997a548d5c076
Dane Cronin <danecronin@gmail.com>
2022-09-27 13:54:50 -0600

    added Heroku

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+  });
+
 
 
 
