3532720f0bc0ace88d38a43a49b51181ba9a2fc4
Dane Cronin <danecronin@gmail.com>
2022-09-27 19:32:03 -0600

    added GET and POST API routes and read/write to db.json

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const uuid = require('uuid');
 
 
 
-
 //Setup Express app/initiate server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Parse(convert) incoming POST data string or array data
+app.use(express.urlencoded({ extended : true}));
+//Parse(convert) incoming JSON data
+app.use(express.json());
 
 //Express.js middleware that instructs server to make front end files available
 app.use(express.static('public'));
@@ -19,10 +23,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-//Parse(convert) incoming POST data string or array data
-app.use(express.urlencoded({ extended : true}));
-//Parse(convert) incoming JSON data
-app.use(express.json());
+
 
 
 //HTML paths
@@ -35,6 +36,40 @@ app.get("/notes", (req, res) => {
 //Wildcard Route to catch unintentional routes and return index
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+//API routes to Get/Post
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+})
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    const newNote = {
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err
+
+        var arrayOfObjects = JSON.parse(data)
+        arrayOfObjects.push(newNote);
+
+        console.log(arrayOfObjects);
+        arrayOfObjects = JSON.stringify(arrayOfObjects);
+        fs.writeFile('./db/db.json', arrayOfObjects, (err, data) => {
+            if (err) throw err
+
+            console.log("Success!");
+        });
+    });
+
+
+    res.send(newNote);
+
 });
 
 
