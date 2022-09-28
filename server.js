b7421adc7d52e8a09511605496b1e9784cfc8f75
Dane Cronin <danecronin@gmail.com>
2022-09-27 18:41:49 -0600

    added json parsing

--- server.js
+++ server.js
@@ -14,10 +14,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+});
+
+//Parse(convert) incoming POST data string or array data
+app.use(express.urlencoded({ extended : true}));
+//Parse(convert) incoming JSON data
+app.use(express.json());
+
 
 //HTML paths
 // RETURN the contents at `notes.html `
@@ -31,11 +37,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query)
-    res.json(results);
-  });
 
 
 
